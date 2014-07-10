1ab9510828526e17fec10719b6fac0db8bc67faf
Dimitri Racordon <kyouko.taiga@gmail.com>
2014-07-10 13:19:27 +0200

    Initial Commit

---
+++ spaction/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  spaction
+//
+//  Created by Dimitri Racordon on 10/07/14.
+//  Copyright (c) 2014 University of Geneva. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
