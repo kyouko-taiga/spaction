e0171ea8798e87ae05df5e2eb891866a2158ca47
Dimitri Racordon <kyouko.taiga@gmail.com>
2014-09-08 11:52:11 +0200

    Added the CLTL translator (work in progress)

--- spaction/include/cra/CltlTranslator.h
+++ spaction/include/cra/CltlTranslator.h
@@ -15,9 +15,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef spaction_CltlTranslator_h
-#define spaction_CltlTranslator_h
+#ifndef SPACTION_INCLUDE_CLTLTRANSLATOR_H_
+#define SPACTION_INCLUDE_CLTLTRANSLATOR_H_
 
+#include <set>
 
+#include "cltl.h"
+#include "cra/CostRegisterAutomaton.h"
 
-#endif
+namespace spaction {
+namespace cra {
+
+/// Helper class to build a Generalized Büchi Automata (GBA) from a CLTL formula.
+///
+/// This class represents a single node within the tree that denotes a set of terms and its
+/// (epsilon-)transition. Each node is a tuple <T,S> where T is a set of terms and S a the set
+/// of successor nodes. Furthermore, each successor is labeled by the sub-alphabet that may lead
+/// to, together with the potential postponed constraints.
+class Node {
+public:
+    typedef std::set<cltl_formula*> Terms;
+    typedef FormulaeSet::iterator TermsIterator;
+    typedef FormulaeSet::const_iterator TermsConstIterator;
+
+    typedef std::pair<Node*, Terms> Successor;
+    typedef std::set<Successor> Successors;
+
+    explicit Node(Terms&& terms);
+
+    Terms terms();
+    TermsIterator terms_iterator();
+    TermsConstIterator terms_iterator() const;
+
+    Successors successors();
+
+};
+
+class CltlTranslator {
+    
+};
+
+}  // namespact cra
+}  // namespact spaction
+
+#endif  // defined SPACTION_INCLUDE_CLTLTRANSLATOR_H_
