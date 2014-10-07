f870762690699870e69b1a889ca3829359f83297
Dimitri Racordon <kyouko.taiga@gmail.com>
2014-10-07 11:23:33 +0200

    Modified the TransitionSystem so it can accept any type for the set of states

--- spaction/include/automata/TransitionSystem.h
+++ spaction/include/automata/TransitionSystem.h
@@ -21,29 +21,28 @@
 namespace spaction {
 namespace automata {
 
-template<typename S> class Transition;
+template<typename Q, typename S> class Transition;
 
 /// Base class for transition systems implementation.
 ///
 /// A transition system is a tuple `<Q,S,T>` where `Q` is a set of states, `S` is an alphabet and
 /// `T` is a a subset of `QxSxQ` representing the transitions.
-template<typename S> class TransitionSystem {
+template<typename Q, typename S> class TransitionSystem {
 public:
     virtual ~TransitionSystem() { }
 
-    virtual void add_state(const std::string &name) = 0;
+    virtual void add_state(const Q &name) = 0;
 
-    virtual Transition<S> *add_transition(const std::string &source, const std::string &sink,
-                                          const S &label) = 0;
+    virtual Transition<Q,S> *add_transition(const Q &source, const Q &sink, const S &label) = 0;
 
-    virtual bool has_state(const std::string &name) const = 0;
+    virtual bool has_state(const Q &name) const = 0;
 
     /// Returns a pointer to an outgoing transition of `source` that is labeled by `label`, unless
     /// returns `nullptr`.
-    virtual Transition<S> *find_transition(const std::string &source, const S &label) const = 0;
+    virtual Transition<Q,S> *find_transition(const Q &source, const S &label) const = 0;
 
     /// Returns a vector of all the outgoing transitions of `source` that are labeled by `label`.
-    virtual std::vector<Transition<S>*> find_all_transitions(const std::string &source,
+    virtual std::vector<Transition<Q,S>*> find_all_transitions(const Q &source,
                                                              const S &label) const = 0;
 
 protected:
@@ -52,11 +51,11 @@ protected:
     ///     Instances of the class Transition shouldn't be constructed outside a TransitionSystem.
     ///     Subclasses may use this method within their implementation of `add_transition` to
     ///     actually create the transitions object.
-    virtual Transition<S> *_make_transition(const std::string &source, const std::string &sink,
+    virtual Transition<Q,S> *_make_transition(const Q &source, const Q &sink,
                                             const S &label) {
         if (!has_state(source) or !has_state(sink))
             return nullptr;
-        return new Transition<S>(source, sink, label);
+        return new Transition<Q,S>(source, sink, label);
     }
 };
 
@@ -64,66 +63,68 @@ protected:
 ///
 /// A deterministic transition system is a transition system where for each state `q` of `Q` and
 /// each symbol `s` of `S`, there is at most one outgoing transition from `q` labeled by `s`.
-template<typename S> class DeterministicTransitionSystem : public TransitionSystem<S> {
+template<typename Q, typename S> class DeterministicTransitionSystem :
+    public TransitionSystem<Q,S> {
 public:
     virtual ~DeterministicTransitionSystem() {}
 
-    virtual void add_state(const std::string &name) {
+    virtual void add_state(const Q &name) {
         if (_graph.count(name) > 0) return;
         _graph[name];
     }
 
-    virtual inline bool has_state(const std::string &name) const {
+    virtual inline bool has_state(const Q &name) const {
         return _graph.count(name) > 0;
     }
 
-    virtual Transition<S> *add_transition(const std::string &source, const std::string &sink,
+    virtual Transition<Q,S> *add_transition(const Q &source, const Q &sink,
                                           const S &label) {
         if (!has_state(source) or !has_state(sink))
             return nullptr;
         
-        Transition<S> *t = this->_make_transition(source, sink, label);
+        Transition<Q,S> *t = this->_make_transition(source, sink, label);
         _graph[source][label] = t;
         return t;
     }
 
-    virtual Transition<S> *find_transition(const std::string &source, const S &label) const {
+    virtual Transition<Q,S> *find_transition(const Q &source, const S &label) const {
         return this->_graph.at(source).at(label);
     }
 
-    virtual std::vector<Transition<S>*> find_all_transitions(const std::string &source,
+    virtual std::vector<Transition<Q,S>*> find_all_transitions(const Q &source,
                                                              const S &label) const {
-        return std::vector<Transition<S>*>({find_transition(source, label)});
+        return std::vector<Transition<Q,S>*>({find_transition(source, label)});
     }
 
 protected:
     /// Stores the transition system as a subset of `QxSxT`.
-    std::unordered_map<std::string, std::unordered_map<S, Transition<S>*>> _graph;
+    std::unordered_map<Q, std::unordered_map<S, Transition<Q,S>*>> _graph;
 };
 
 /// Class that stores an undeterministic transition system.
 ///
 /// A undeterministic transition system is a transition system where for each state `q` of `Q` and
 /// each symbol `s` of `S`, there can be more than one outgoing transition from `q` labeled by `s`.
-template<typename S> class UndeterministicTransitionSystem : public TransitionSystem<S> {
+template<typename Q, typename S> class UndeterministicTransitionSystem :
+    public TransitionSystem<Q,S> {
 public:
     virtual ~UndeterministicTransitionSystem() {}
     
-    virtual void add_state(const std::string &name) {
+    virtual void add_state(const Q &name) {
         if (_graph.count(name) > 0) return;
         _graph[name];
     }
 
-    virtual inline bool has_state(const std::string &name) const {
+    virtual inline bool has_state(const Q &name) const {
         return _graph.count(name) > 0;
     }
 
-    virtual Transition<S> *add_transition(const std::string &source, const std::string &sink,
+    virtual Transition<Q,S> *add_transition(const Q &source, const Q &sink,
                                           const S &label) {
         if (!has_state(source) or !has_state(sink))
             return nullptr;
         
-        Transition<S> *t = this->_make_transition(source, sink, label);
+        Transition<Q,S> *t = this->_make_transition(source, sink, label);
         _graph[source][label].push_back(t);
         return t;
     }
@@ -131,35 +132,35 @@ public:
     /// @warning
     ///     If `source` has several outgoing transitions labeled by `label`, this method will return
     ///     the first it will find.
-    virtual Transition<S> *find_transition(const std::string &source, const S &label) const {
+    virtual Transition<Q,S> *find_transition(const Q &source, const S &label) const {
         return this->_graph.at(source).at(label).at(0);
     }
 
-    virtual std::vector<Transition<S>*> find_all_transitions(const std::string &source,
+    virtual std::vector<Transition<Q,S>*> find_all_transitions(const Q &source,
                                                              const S &label) const {
         return this->_graph.at(source).at(label);
     }
 
 protected:
     /// Stores the transition system as a subset of `QxSx<QxSx<T*>>`.
-    std::unordered_map<std::string, std::unordered_map<S, std::vector<Transition<S>*>>> _graph;
+    std::unordered_map<Q, std::unordered_map<S, std::vector<Transition<Q,S>*>>> _graph;
 };
 
 /// Class that represents a transition in a TransitionSystem.
-template <typename S> class Transition {
+template <typename Q, typename S> class Transition {
 public:
-    const std::string &source() const { return _source; }
-    const std::string &sink()   const { return _sink; }
+    const Q &source() const { return _source; }
+    const Q &sink()   const { return _sink; }
     const S &label()       const { return _label; }
 
 protected:
-    friend class TransitionSystem<S>;
+    friend class TransitionSystem<Q,S>;
 
-    const std::string _source;
-    const std::string _sink;
+    const Q _source;
+    const Q _sink;
     const S _label;
 
-    explicit Transition(const std::string &source, const std::string &sink, const S &label) :
+    explicit Transition(const Q &source, const Q &sink, const S &label) :
     _source(source), _sink(sink), _label(label) {
     }
 
